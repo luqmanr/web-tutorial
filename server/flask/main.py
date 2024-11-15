190afffba932c4b8d0114b9b7b58acd0db165f4a
Luqman Rahardjo <luqman.rahardjo@gmail.com>
2024-11-16 05:59:15 +0700

    read csv

--- server/flask/main.py
+++ server/flask/main.py
@@ -1,7 +1,9 @@
 import jsonpickle
 import pandas as pd 
+import csv
+import os
 
-from aioflask import Flask, Response, request, redirect, url_for, render_template 
+from flask import Flask, Response, request, redirect, url_for, render_template 
 from flask_cors import CORS
 
 
@@ -24,12 +26,12 @@ cors = CORS(app)
 #     return html_template
 # '''
 @app.route('/index.html', methods = ['GET'])
-async def serve_index():
+def serve_index():
     f = open('index.html', 'r')
     return f.read()
 # '''
 @app.route('/contoh_function', methods = ['GET'])
-async def contoh_function():
+def contoh_function():
     response = {
         "status": 200,
         "status_message": "contoh_function OK"
@@ -38,7 +40,7 @@ async def contoh_function():
     return Response(jsonpickle.encode(response), mimetype="application/json", status=response['status'])
 
 @app.route('/submit_form', methods = ['GET', 'POST'])
-async def submit_form():
+def submit_form():
     req_headers = dict(request.headers)
     print(f'headers: {req_headers}')
 
@@ -49,6 +51,14 @@ async def submit_form():
     print(f'data/body: {req_data}')
 
     # we can then do something with the variable `req_headers` & `req_params`
+    csv_path = 'csv_file.csv'
+    if not os.path.exists(csv_path):
+        csv_file = open(csv_path, 'w')
+        csv_file.write('fname,lname\n')
+        csv_file.close()
+    csv_file = open(csv_path, 'a')
+    csv_file.write(f'{req_params["fname"]},{req_params["lname"]}\n')
+    csv_file.close()
 
     response = {
         "status": 200,
@@ -61,11 +71,33 @@ async def submit_form():
     return Response(jsonpickle.encode(response), mimetype="application/json", status=response['status'])
 
 @app.route('/table') 
-async def table(): 
+def table(): 
     # converting csv to html 
     data = pd.read_csv('sample_data.csv') 
     print(data.to_html())
-    resp = await render_template('table.html', tables=[data.to_html()], titles=['']) 
+    resp = render_template('table.html', tables=[data.to_html()], titles=['']) 
+    return resp
+
+@app.route('/csv') 
+def csv_read(): 
+    # we can then do something with the variable `req_headers` & `req_params`
+    csv_path = 'csv_file.csv'
+    if not os.path.exists(csv_path):
+        return 'doesn\'t exist'
+    csv_data = open(csv_path, 'r').read()
+    csv_headers = []
+    rows = []
+    l = 0
+    for line in csv_data.split('\n'):
+        print(line)
+        if l == 0:
+            csv_headers = line.split(',')
+        else:
+            rows.append(line.split(','))
+        l += 1
+    print(csv_headers)
+    print(rows)
+    resp =''
     return resp
   
 if __name__ == '__main__':
