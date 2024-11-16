2ddd598585ad6c9a6b2696700ef3dd69fd6e538f
luqman.rahardjo@gmail.com <luqman.rahardjo@gmail.com>
2024-11-16 09:07:40 +0700

    change port

--- server/flask/main.py
+++ server/flask/main.py
@@ -100,5 +100,5 @@ def csv_read():
   
 if __name__ == '__main__':
     print("Starting server...")
-    port = "9999"
+    port = "5000"
     app.run(host='0.0.0.0', port=port, threaded=True, debug=True)
