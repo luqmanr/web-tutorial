cc2fa2f4fa97dc0392108d777aa1fb17f918170c
luqmanr <luqman.rahardjo@gmail.com>
2025-06-21 19:02:51 +0700

    Update chat.py

--- belajar/API/chat.py
+++ belajar/API/chat.py
@@ -1,7 +1,7 @@
 import requests
 
 # go to https://aistudio.google.com/app/apikey
-API_KEY = 'AIzaSyDcNMnHAH9cGZSay0vSobaoQTm5gJhP4Nw'
+API_KEY = 'ar0w3jf0s9jf0s9jf09sjd09fjsdf'
 URL = f'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={API_KEY}'
 headers = {
     'Content-Type': 'application/json'
